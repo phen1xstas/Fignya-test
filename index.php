d2f7b06dc6470c50f1e398a1e5210a76ace47498
phen1xstas <stas.afanasev@gmail.com>
2019-01-19 02:56:24 +0300

    Fignya5
    Я наконец-то залил эту ересь гитхаб

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$arr = [
+    'key1' => 'value1',
+    'key2' => 'value2',
+    'key3' => 'value3',
+    'key4' => 'value4',
+];
+
+foreach ($arr as $a => $b) {
+    echo $a . ': ' . $b . '<br>';
+}
